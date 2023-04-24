8f5309ab8f0e594fa770dd8fafe3e18593298832
SmitAkbari26 <akbarismit26@gmail.com>
2023-04-24 12:07:26 +0530

    Generate Word File Event Wise

--- organizer/generateWord.php
+++ organizer/generateWord.php
@@ -1,4 +1,7 @@
 <?php
+
+include('../include/db_conn.php');
+
 require_once './php-word/vendor/autoload.php'; // Include the autoload.php file from phpoffice/phpword
 // Create a new Word document
 $phpWord = new \PhpOffice\PhpWord\PhpWord();
@@ -6,16 +9,28 @@ $phpWord = new \PhpOffice\PhpWord\PhpWord();
 // Add a section to the document
 $section = $phpWord->addSection();
 
-// Add some text to the section
-$section->addText('Event Details:', ['bold' => true, 'size' => 16]);
-$section->addText('Name: John Doe');
-$section->addText('ID: 123456');
-$section->addText('Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-
-// Save the document as a Word file
-$filename = 'event_details.docx';
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment;filename="'. $filename .'"');
-$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-$objWriter->save('php://output');
-exit();
+$festName = $_GET['festName'];
+$eventId = $_GET['id'];
+if ($festName) {
+    $query = "SELECT * FROM `$festName` WHERE `id` = '$eventId'";
+    $eventResult = mysqli_query($conn, $query);
+    if (mysqli_num_rows($eventResult) > 0) {
+        $eventRow = mysqli_fetch_assoc($eventResult);
+        // Add some text to the section
+        $section->addText('' . $festName . '', ['bold' => true, 'size' => 28]);
+        $section->addText('EventID:' . $eventRow['eventId'] . '');
+        $section->addText('EventName:' . $eventRow['eventName'] . '');
+        $section->addText('EventFaculty:' . $eventRow['eventFaculty'] . '');
+        $section->addText('EventMembers:' . $eventRow['eventMembers'] . '');
+        $section->addText('EventDescription: ' . $eventRow['eventDescription'] . '');
+        // Save the document as a Word file
+        $filename = $eventRow['eventName'].".docx";
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+        $objWriter->save('php://output');
+        exit();
+    }
+} else {
+    header('Location: organizer.php?error=Something went wrong.F');
+}
--- organizer/organizer.php
+++ organizer/organizer.php
@@ -2,7 +2,7 @@
 session_start();
 
 include('../include/head.php');
-include('../include/loading.php');
+// include('../include/loading.php');
 include('../include/db_conn.php');
 include('../include/header.php');
 ?>
@@ -11,89 +11,96 @@ include('../include/header.php');
     <a href="generateExcelParticipants.php" class="btn">Excel Participant</a>
     <a href="generateExcelVolunteer.php" class="btn">Excel Volunteer</a>
     <a href="selectService.php" class="btn">Add Fests</a>
-    <a href="generateWord.php" class="btn">Word</a>
 </div>
-<div class="load">
-    <?php
-    $id = $_SESSION['id'];
-    $query = "SELECT * FROM `fests` WHERE `userId` = '$id'";
-    $result = mysqli_query($conn, $query);
-    echo "<h1 class='text-center mt-5 my-2'>Events</h1>";
-    echo "<center><hr class='w-50'/></center>";
-    if ($result && mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $festName = $row['festName'];
-            echo "<h2 align='center'>$festName</h2>";
-            if ($festName) {
 
-                $query = "SELECT * FROM `$festName`";
-                $eventResult = mysqli_query($conn, $query);
-                if (mysqli_num_rows($eventResult) > 0) {
-    ?>
-                    <table class="table table-striped table-hover">
-                        <thead class="text-center">
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">eventId</th>
-                                <th scope="col">eventName</th>
-                                <th scope="col">eventDescription</th>
-                                <th scope="col">eventFaculty</th>
-                                <th scope="col">eventMembers</th>
-                                <?php
-                                echo "<th>
+<?php
+$id = $_SESSION['id'];
+$query = "SELECT * FROM `fests` WHERE `userId` = '$id'";
+$result = mysqli_query($conn, $query);
+echo "<h1 class='text-center mt-5 my-2'>Events</h1>";
+echo "<center><hr class='w-50'/></center>";
+if ($result && mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $festName = $row['festName'];
+        echo "<h2 align='center'>$festName</h2>";
+        if ($festName) {
+
+            $query = "SELECT * FROM `$festName`";
+            $eventResult = mysqli_query($conn, $query);
+            if (mysqli_num_rows($eventResult) > 0) {
+?>
+                <table class="table table-striped table-hover">
+                    <thead class="text-center">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">eventId</th>
+                            <th scope="col">eventName</th>
+                            <th scope="col">eventDescription</th>
+                            <th scope="col">eventFaculty</th>
+                            <th scope="col">eventMembers</th>
+                            <?php
+                            echo "<th>
                                 <form method='POST' style='box-shadow: none;' action='festEvent.php?festName=" . $festName . "'>
                                 <button type='submit' class='btn' name='add-event'>Add Event</button>
                                 </form>
                                 </th>";
-                                echo "<th>
+                            echo "<th>
                                 <form method='POST' style='box-shadow: none;' onsubmit='return confirmDelete();' action='deleteFest.php?festName=" . $festName . "'>
                                 <button type='submit' class='btn-v' name='add-event'>Delete Fest</button>
                                 </form>
                                 </th>";
-                                ?>
-                            </tr>
-                        </thead>
-                        <tbody class="text-center">
-                            <?php
-                            while ($eventRow = mysqli_fetch_assoc($eventResult)) {
-                                echo "<tr>";
-                                echo "<td>" . $eventRow['id'] . "</td>";
-                                echo "<td>" . $eventRow['eventId'] . "</td>";
-                                echo "<td>" . $eventRow['eventName'] . "</td>";
-                                echo "<td>" . $eventRow['eventDescription'] . "</td>";
-                                echo "<td>" . $eventRow['eventFaculty'] . "</td>";
-                                echo "<td>" . $eventRow['eventMembers'] . "</td>";
-                                echo "<td>
+                            ?>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                        <?php
+                        while ($eventRow = mysqli_fetch_assoc($eventResult)) {
+                            echo "<tr>";
+                            echo "<td>" . $eventRow['id'] . "</td>";
+                            echo "<td>" . $eventRow['eventId'] . "</td>";
+                            echo "<td>" . $eventRow['eventName'] . "</td>";
+                            echo "<td>" . $eventRow['eventDescription'] . "</td>";
+                            echo "<td>" . $eventRow['eventFaculty'] . "</td>";
+                            echo "<td>" . $eventRow['eventMembers'] . "</td>";
+                            echo "<td>
                                 <form method='POST' style='box-shadow: none;' action='updateFestEvent.php?id=" . $eventRow['id'] . "&&festName=" . $festName . "'>
-                                <button type='submit' style='background-color:#3ae374; border:2px solid ##3ae374; color:#fff;'  class='btn' name='update'>Update</button>
+                                <button type='submit' style='background-color:#3ae374; border:2px solid #3ae374; color:#fff;'  class='btn-v' name='update'>Update</button>
                                 </form>
                             </td>";
-                                echo "<td>
+                            echo "<td>
                                     <form method='POST' style='box-shadow: none;' onsubmit='return confirmDelete();' action='deleteFestEvent.php?id=" . $eventRow['id'] . "&&festName=" . $festName . "'>
                                     <button type='submit' class='btn-v' style='background-color:#ff3838; border:2px solid #ff3838; color:#fff;' name='delete'>Delete</button>
                                     </form>
                                 </td>";
-                                echo "</tr>";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
+                            echo "<td>
+                                    <form method='POST' style='box-shadow: none;' action='generateWord.php?id=" . $eventRow['id'] . "&&festName=" . $festName . "'>
+                                    <button type='submit' class='btn-v' style='background-color:#17c0eb; border:2px solid #17c0eb; color:#fff;' name='delete'>Generate Word</button>
+                                    </form>
+                                </td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
     <?php
-                } else {
-                    echo "<center>No events found for .$festName.</center>";
-                }
             } else {
-                header('Location: organizer.php?error=Something went wrong.F');
+                echo "<center>No events found for .$festName.</center>";
             }
+        } else {
+            header('Location: organizer.php?error=Something went wrong.F');
         }
-    } else {
-        header('Location: selectService.php');
     }
+} else {
     ?>
-    <?php
-    include('users.php');
-    include('../include/footer.php');
-    ?>
-</div>
+    <script>
+        window.location.href = "http://localhost/organizer/selectService.php";
+    </script>
+<?php
+}
+?>
+<?php
+include('users.php');
+include('../include/footer.php');
+?>
 <?php
 include('../include/scripts.php');
